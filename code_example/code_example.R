fedbf22729bac9abb7de657b3e81f130a43075b7
Jadon Wagstaff <jadonw@gmail.com>
2022-08-11 20:11:00 -0600

    Revision after adding SE to remap.

--- code_example/code_example.R
+++ code_example/code_example.R
@@ -7,7 +7,6 @@ library(remap)
 
 
 # Load data
-# ==============================================================================
 # Download data from https://github.com/jadonwagstaff/remap_manuscript_code/tree/master/code_example
 load("code_example_data.RData")
 
@@ -17,11 +16,13 @@ eco3_simp <- eco3 %>%
   dplyr::filter(!sf::st_is_empty(.)) %>%
   sf::st_cast("MULTIPOLYGON")
 
+names(eco3_simp)
+
 # Make distance matrix
 eco3_dist <- redist(loads, regions = eco3_simp, region_id = ECO3, progress = TRUE)
 
 
-
+# Linear model
 lmod <- remap(loads,
               regions = eco3_simp, region_id = ECO3,
               buffer = 50, min_n = 150,
@@ -30,37 +31,61 @@ lmod <- remap(loads,
               formula = log(EVENT50) ~ ELEVATION,
               progress = TRUE)
 
+lmod
+
+head(names(lmod$models), 3)
+
+class(lmod$models[[1]])
+
+head(lmod$regions, 3)
+
+
+# GAM
+gam_wrap <- function(data, ...) {
+  model <- mgcv::gam(data, ...)
+  class(model) <- "gam_wrap"
+  return(model)
+}
+
+predict.gam_wrap <- function(object, data, se.fit = FALSE) {
+  class(object) <- "gam"
+  if (se.fit) {
+    # return standard errors
+    return(predict(object, data, se.fit = TRUE)$se.fit)
+  } else {
+    # return predictions
+    return(predict(object, data))
+  }
+}
 
 gm <- remap(loads,
-             regions = eco3_simp, region_id = ECO3,
-             buffer = 50, min_n = 150,
-             distances = eco3_dist,
-             model_function = mgcv::gam,
-             formula = log(EVENT50) ~ s(ELEVATION, k = 15) +
-               s(LATITUDE, LONGITUDE, bs = 'sos', k = 75),
-             family = gaussian,
-             progress = TRUE)
+            regions = eco3_simp, region_id = ECO3,
+            buffer = 50, min_n = 150,
+            distances = eco3_dist,
+            model_function = gam_wrap,
+            formula = log(EVENT50) ~ s(ELEVATION, k = 15) +
+              s(LATITUDE, LONGITUDE, bs = 'sos', k = 75),
+            family = gaussian,
+            progress = TRUE)
 
-gm
+predict(gm, loads[1:3, ], smooth = 25)
 
-head(names(gm$models), 3)
+predict(gm, loads[1:3, ], smooth = 25, se = TRUE, se.fit = TRUE)
 
-class(gm$models[[1]])
 
-head(gm$regions, 3)
 
 
 
 
 # Kriging model functions
-# ==============================================================================
-krig <- function(data, fml) {
+projection <- sf::st_crs("+proj=laea +x_0=0 +y_0=0 +lon_0=-100 +lat_0=45")
+
+krig <- function(data, formula) {
   data <- data %>%
-    sf::st_transform(sf::st_crs("+proj=laea +x_0=0 +y_0=0
-                                +lon_0=-100 +lat_0=45")) %>%
+    sf::st_transform(projection) %>%
     sf::as_Spatial()
 
-  out <- list(data = data, fml = fml)
+  out <- list(data = data, formula = formula)
   class(out) <- "krig"
 
   return(out)
@@ -69,15 +94,18 @@ krig <- function(data, fml) {
 predict.krig <- function(object, data) {
   if (nrow(data) != 0) {
     data <- data %>%
-      sf::st_transform(sf::st_crs("+proj=laea +x_0=0 +y_0=0
-                                  +lon_0=-100 +lat_0=45")) %>%
+      sf::st_transform(projection) %>%
       sf::as_Spatial()
 
-    variogram_object <- automap::autofitVariogram(object$fml, object$data,
-                                                  model = "Sph")
+    variogram_object <- automap::autofitVariogram(
+      formula = object$formula,
+      input_data = object$data,
+      model = "Sph")
 
-    k <- gstat::krige(object$fml, object$data,
-                      data, variogram_object$var_model,
+    k <- gstat::krige(formula = object$formula,
+                      locations = object$data,
+                      newdata = data,
+                      model = variogram_object$var_model,
                       debug.level = 0)
 
     return(k$var1.pred)
@@ -90,7 +118,7 @@ kg <- remap(loads,
             buffer = 50, min_n = 150,
             distances = eco3_dist,
             model_function = krig,
-            fml = log(EVENT50) ~ ELEVATION,
+            formula = log(EVENT50) ~ ELEVATION,
             progress = TRUE)
 
 loads_us <- sf::st_filter(loads, cont_us)
@@ -151,7 +179,7 @@ utkg <- remap(utsp2011,
               regions = utws, region_id = HUC2,
               buffer = 20, min_n = 30,
               model_function = krig,
-              fml = log(WESD) ~ ELEVATION,
+              formula = log(WESD) ~ ELEVATION,
               progress = TRUE)
 
 
